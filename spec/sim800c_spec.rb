d86c6ab3de0ec6cf5954a6e10171aa4d8320f0d7
Nujian Den Mark Meralpis <denmarkmeralpis@gmail.com>
2025-06-11 12:48:13 +0800

    remove unused spec

--- spec/sim800c_spec.rb
+++ spec/sim800c_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Sim800c do
   it "has a version number" do
     expect(Sim800c::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
